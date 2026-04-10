e89c67af6c3f7255fe3ade77bb065fed9cb13b12
Dania Danial <daniaisead2002@gmail.com>
2026-04-10 17:28:14 +0300

    python 1

---
+++ _python/assigments/hello_world.py
@@ -0,0 +1,17 @@
+print("Hello World")
+name ="Dania"
+print("Hello " , name)
+print("Hello " + name)
+print(f"Hello {name}!")
+
+number = 42
+print("Hello " , number)
+print("Hello " + number)#error 
+ #to solve
+print("Hello "+ str(number))
+
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}")
