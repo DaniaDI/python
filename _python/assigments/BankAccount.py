2a09385d737f10c665df0b4678ff0ba79ae1b4f8
Dania Danial <daniaisead2002@gmail.com>
2026-04-18 17:09:39 +0300

    bank2

--- _python/assigments/BankAccount.py
+++ _python/assigments/BankAccount.py
@@ -23,9 +23,34 @@ class  BankAccount:
         if self.balance > 0:
             self.balance += self.balance * self.int_rate
         return self
+
+
+class User:
+        def __init__(self, name, email):
+            self.name = name
+            self.email = email
+            self.account = BankAccount(0.02, 0)
+
+
+        def make_deposit(self, amount):
+            self.account.deposit(amount)
+            return self
+
+        def make_withdrawal(self, amount):
+            self.account.withdraw(amount)
+            return self
+
+        def display_user_balance(self):
+            print(f"User: {self.name}, Balance: ${self.account.balance}")
+            return self
     
+
 account1 = BankAccount(0.01, 100)
 account1.deposit(50).deposit(50).deposit(50).withdraw(30).yield_interest().display_account_info()
  
 account2 = BankAccount(0.02, 200)
 account2.deposit(100).deposit(50).withdraw(30).withdraw(20).withdraw(10).withdraw(5).yield_interest().display_account_info()
+
+user1 = User("Dania", "dania@email.com")
+
+user1.make_deposit(100).make_deposit(50).make_withdrawal(30).display_user_balance()
