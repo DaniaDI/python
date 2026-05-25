34b021a48e53c1bcded2f0c33c61f9db167bfd6c
Dania Danial <daniaisead2002@gmail.com>
2026-05-26 00:19:03 +0300

    dj-greate_num_game

---
+++ django/django_fundamentals/Greate_num_game/greate_num_app/views.py
@@ -0,0 +1,49 @@
+
+from django.shortcuts import render, redirect
+import random
+
+def index(request):
+   
+    if "num" not in request.session:
+        request.session["num"] = random.randint(1, 100)
+        request.session["game_over"] = False
+        request.session["attempts"] = 0
+        request.session["result"] = ""
+
+   
+    context = {
+        "result": request.session.get("result"),
+        "game_over": request.session.get("game_over"),
+        "attempts": request.session.get("attempts"),
+    }
+
+  
+    return render(request, "index.html", context)
+
+
+def guess(request):
+    user_input = int(request.POST["num"])
+    server = int(request.session["num"])
+    request.session["attempts"] = request.session.get("attempts", 0) + 1
+
+   
+    if request.session["attempts"] >= 5 and user_input != server:
+        request.session["result"] = "You Lose! The number was " + str(server)
+        request.session["game_over"] = True
+    else:
+        
+        if user_input < server:
+            request.session["result"] = "Too Low"
+        elif user_input > server:
+            request.session["result"] = "Too High"
+        else:
+            request.session["result"] = "Correct!"
+            request.session["game_over"] = True
+
+    return redirect("/")
+
+
+def restart(request):
+  
+    request.session.flush()
+    return redirect("/")
