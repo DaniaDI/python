7ed1c927da6538660aa7bd8ac734205827fdc715
Dania Danial <daniaisead2002@gmail.com>
2026-06-03 15:32:39 +0300

    dojo_ninja_template_orm

--- django/django_orm/dojo_ninja_proj/dojo_ninja_app/urls.py
+++ django/django_orm/dojo_ninja_proj/dojo_ninja_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-
-    path('',views.index,name='index')
+    path('',views.index,name='dojo_index'),
+    path('add_dojo/', views.add_dojo, name='add_dojo'),
+    path('add_ninja/', views.add_ninja, name='add_ninja'),
+    path('delete/<int:id>/', views.delete_dojo, name='delete_dojo'),
 
 ]
--- django/django_orm/dojo_ninja_proj/dojo_ninja_app/views.py
+++ django/django_orm/dojo_ninja_proj/dojo_ninja_app/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render,redirect
-
+from .models import Dojo, Ninja
 # Create your views here.
 def index(request):
-  return render(request,'index.html')
+   context = {
+        'dojos': Dojo.objects.all()# ninjas من خلال dojo ,related_name=ninjas
+    }
+   return render(request, 'index.html', context)
+  
+
+def add_dojo(request):
+    if request.method == 'POST':
+        name=request.POST['name']
+        city  = request.POST['city']
+        state = request.POST['state']
+       
+        Dojo.objects.create(
+            name  = name,
+            city  = city,
+            state = state,
+            desc  = 'new_dojo',
+        )
+    return redirect('/')
+def add_ninja(request):
+    if request.method == 'POST':
+        f_name=request.POST['first_name']
+        l_name  = request.POST['last_name']
+        dojo_id  = request.POST['dojo_id']
+        Ninja.objects.create(
+            first_name  = f_name,
+            last_name  = l_name,
+            dojo = Dojo.objects.get(id=dojo_id),
+          
+        )
+    return redirect('/')
+
+def delete_dojo(request,id):
+    if request.method == 'POST':
+      Dojo.objects.get(id=id).delete()  # ← بتحذف الـ dojo + كل ninjas تبعه
+    return redirect('/')
